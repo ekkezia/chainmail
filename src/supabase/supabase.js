9d70dc2ae13f862827a77d7ab5c20e420afc52ce
Elizabeth Kezia <91784486+ekkezia@users.noreply.github.com>
2024-10-30 10:26:49 +0800

    chore: Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL || "https://lmgbcuolwhkqoowxnaik.supabase.co";
+export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 
-export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+export const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export default createClient(
     supabaseUrl,
